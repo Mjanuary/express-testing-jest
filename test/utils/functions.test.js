e9f18cec7ded314ba82e45e51c1b19d5e0722b4f
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2022-12-12 10:54:15 +0200

    Rename changes

--- test/utils/functions.test.js
+++ test/utils/functions.test.js
@@ -1,8 +1,8 @@
 const { getPage } = require("../../utils/functions");
 
-describe("It should return 44 when it receive getPage(12,4)", () => {
+describe("Testing getPage() function", () => {
   const getPageResult = getPage(12, 4);
-  it("should uptate a product.", () => {
+  it("It should return 44 when it receive getPage(12,4)", () => {
     expect(getPageResult).toBe(44);
   });
 });
